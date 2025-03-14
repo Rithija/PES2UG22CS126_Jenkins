1a5626083956a1127457d675d01781609b92ae9f
unknown <rithu23ps@gmail.com>
2025-03-14 15:38:31 +0530

    commiting the lab 08

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout<<"Lab 8 documentation has been so bad, I want to scream";
+  return 0;
+}
